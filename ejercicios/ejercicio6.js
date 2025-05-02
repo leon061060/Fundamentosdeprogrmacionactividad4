a9aee5c95938b58a2c44c0a7d89f5a912dc2f76d
leon061060 <leonhernandesleidy@gmail.com>
2025-05-02 18:52:51 -0500

    cambio7

--- ejercicios/ejercicio6.js
+++ ejercicios/ejercicio6.js
@@ -6,6 +6,10 @@ output: process.stdout
 });
 
 rl.question('Escribe un número entre 0 y 10: ', function(respuesta) {
-    const numero = parseInt(respuesta);)}
-    
+    const numero = parseInt(respuesta);
+
+    if (isNaN(numero) || numero < 0 || numero > 10) {
+        console.log('¡Error! Debes escribir un número del 0 al 10.');
+      } 
+
 
