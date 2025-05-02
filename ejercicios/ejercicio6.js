b5f2ec9422f15e6527887e4ffe13a1522fd53969
leon061060 <leonhernandesleidy@gmail.com>
2025-05-02 18:48:30 -0500

    cambio6

--- ejercicios/ejercicio6.js
+++ ejercicios/ejercicio6.js
@@ -4,3 +4,8 @@ const rl=readline.createinterface({
 input: process.stdin,
 output: process.stdout
 });
+
+rl.question('Escribe un número entre 0 y 10: ', function(respuesta) {
+    const numero = parseInt(respuesta);)}
+    
+
