cdaee4dd9ea2c7c6836f1cb96bbcff5b3fb28a27
leon061060 <leonhernandesleidy@gmail.com>
2025-05-02 19:33:33 -0500

    cambio8

--- ejercicios/ejercicio6.js
+++ ejercicios/ejercicio6.js
@@ -10,6 +10,12 @@ rl.question('Escribe un número entre 0 y 10: ', function(respuesta) {
 
     if (isNaN(numero) || numero < 0 || numero > 10) {
         console.log('¡Error! Debes escribir un número del 0 al 10.');
-      } 
-
-
+      }else {
+        console.log(`\nTabla del ${numero}:`);
+        // Mostramos la tabla de multiplicar del número
+        for (let i = 1; i <= 10; i++) {
+          console.log(`${numero} x ${i} = ${numero * i}`);
+        }
+    }
+    rl.close();
+});
