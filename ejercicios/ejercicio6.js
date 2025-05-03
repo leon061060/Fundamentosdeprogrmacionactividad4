33405437b700ce4d373dde72a7211333eb746eb8
leon061060 <leonhernandesleidy@gmail.com>
2025-05-02 19:58:57 -0500

    cambio9

--- ejercicios/ejercicio6.js
+++ ejercicios/ejercicio6.js
@@ -1,6 +1,6 @@
 const readline = require('readline');
 
-const rl=readline.createinterface({
+const rl=readline.createInterface({
 input: process.stdin,
 output: process.stdout
 });
@@ -12,7 +12,7 @@ rl.question('Escribe un número entre 0 y 10: ', function(respuesta) {
         console.log('¡Error! Debes escribir un número del 0 al 10.');
       }else {
         console.log(`\nTabla del ${numero}:`);
-        // Mostramos la tabla de multiplicar del número
+
         for (let i = 1; i <= 10; i++) {
           console.log(`${numero} x ${i} = ${numero * i}`);
         }
